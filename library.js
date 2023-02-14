9d589ba2215de942860ddbfdca55906746a08ad6
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-02-13 23:09:01 -0800

    Add card info from array

--- library.js
+++ library.js
@@ -13,7 +13,22 @@ closeModal.addEventListener('click', () => {
 
 
 // eslint-disable-next-line prefer-const
-let myLibrary = [];
+let myLibrary = [
+  {
+    title: 'Dracula',
+    author: 'Bram Stoker',
+    pages: '418',
+    published: '1897',
+    status: 'Not Read'
+  },
+  {
+    title: 'Don Quixote',
+    author: 'Miguel de Cervantes',
+    pages: '1072',
+    published: '1605',
+    status: 'Not Read'
+  }
+];
 
 function Book(title, author, pages, published, status) {
   this.title = title;
@@ -38,6 +53,7 @@ addBookForm.addEventListener('submit', (e) => {
   const book = new Book(title, author, pages, publishedDate, readStatus);
   myLibrary.push(book);
   addBookForm.reset();
+  iterateBooks();
   modal.close();
 });
 
@@ -48,16 +64,69 @@ function iterateBooks() {
   for (book of myLibrary) {
     const unreadCard = document.createElement('div');
     unreadCard.classList.add('unread-card')
+    booksMain.appendChild(unreadCard);
+
     const bookInfo = document.createElement('div');
     bookInfo.classList.add('book-info');
-    const cardButtons = document.createElement('div');
-    cardButtons.classList.add('card-buttons');
+    unreadCard.appendChild(bookInfo);
+
     const bookTitle = document.createElement('p');
     bookTitle.classList.add('book-title');
-
-    booksMain.appendChild(unreadCard);
-    unreadCard.appendChild(bookInfo);
-    unreadCard.appendChild(cardButtons);
     bookInfo.appendChild(bookTitle);
+
+    const divider = document.createElement('hr');
+    bookInfo.appendChild(divider);
+
+    const bookAuthorContainer = document.createElement('p');
+    bookInfo.appendChild(bookAuthorContainer);
+    bookAuthorContainer.textContent = 'By: ';
+    const bookAuthor = document.createElement('span');
+    bookAuthor.classList.add('book-author');
+    bookAuthorContainer.appendChild(bookAuthor);
+
+    const bookPagesContainer = document.createElement('p');
+    bookInfo.appendChild(bookPagesContainer);
+    bookPagesContainer.textContent = 'Pages: ';
+    const bookPages = document.createElement('span');
+    bookPages.classList.add('book-pages');
+    bookPagesContainer.appendChild(bookPages);
+
+    const bookPublishedContainer = document.createElement('p');
+    bookInfo.appendChild(bookPublishedContainer);
+    bookPublishedContainer.textContent = 'Published in: ';
+    const bookPublished = document.createElement('span');
+    bookPublished.classList.add('book-published');
+    bookPublishedContainer.appendChild(bookPublished);
+
+    const cardButtons = document.createElement('div');
+    cardButtons.classList.add('card-buttons');
+    unreadCard.appendChild(cardButtons);
+
+    const notReadButton = document.createElement('button');
+    cardButtons.appendChild(notReadButton);
+    notReadButton.classList.add('not-read-btn');
+    notReadButton.textContent = 'Not Read';
+
+    const editButton = document.createElement('button');
+    cardButtons.appendChild(editButton);
+    editButton.classList.add('edit-btn');
+    const editIcon = document.createElement('i');
+    editButton.appendChild(editIcon);
+    editIcon.classList.add('fa-solid');
+    editIcon.classList.add('fa-pen-to-square');
+
+    const deleteButton = document.createElement('button');
+    cardButtons.appendChild(deleteButton);
+    deleteButton.classList.add('delete-btn');
+    const deleteIcon = document.createElement('i');
+    deleteButton.appendChild(deleteIcon);
+    deleteIcon.classList.add('fa-solid');
+    deleteIcon.classList.add('fa-xmark');
+    
+    const bookIndex = myLibrary.indexOf(book);
+    bookTitle.textContent = myLibrary[bookIndex].title;
+    bookAuthor.textContent = myLibrary[bookIndex].author;
+    bookPages.textContent = myLibrary[bookIndex].pages;
+    bookPublished.textContent = myLibrary[bookIndex].published;
   }
 }
